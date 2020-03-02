c762bbd32c3cced7df0ba61a93c95ab4e265de5c
Necronmat <elsmithyomatt@aol.com>
2020-03-02 00:35:43 +0000

    Initial ReCommit
    Re commoting to make sure this can be seen to be marked

---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Game : MonoBehaviour
+{
+    [SerializeField] private Camera MainCamera;
+    [SerializeField] private Character Character;
+    [SerializeField] private Canvas Menu;
+    [SerializeField] private Canvas Hud;
+    [SerializeField] private Transform CharacterStart;
+
+    private RaycastHit[] mRaycastHits;
+    private Character mCharacter;
+    private Environment mMap;
+
+    private readonly int NumberOfRaycastHits = 1;
+
+    void Start()
+    {
+        mRaycastHits = new RaycastHit[NumberOfRaycastHits];
+        mMap = GetComponentInChildren<Environment>();
+        mCharacter = Instantiate(Character, transform);
+        ShowMenu(true);
+    }
+
+    private void Update()
+    {
+        // Check to see if the player has clicked a tile and if they have, try to find a path to that 
+        // tile. If we find a path then the character will move along it to the clicked tile. 
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray screenClick = MainCamera.ScreenPointToRay(Input.mousePosition);
+            int hits = Physics.RaycastNonAlloc(screenClick, mRaycastHits);
+            if( hits > 0)
+            {
+                EnvironmentTile tile = mRaycastHits[0].transform.GetComponent<EnvironmentTile>();
+                if (tile != null)
+                {
+                    List<EnvironmentTile> route = mMap.Solve(mCharacter.CurrentPosition, tile);
+                    mCharacter.GoTo(route);
+                }
+            }
+        }
+    }
+
+    public void ShowMenu(bool show)
+    {
+        if (Menu != null && Hud != null)
+        {
+            Menu.enabled = show;
+            Hud.enabled = !show;
+
+            if (show)
+            {
+                mMap.CleanUpWorld();
+            }
+            else
+            {               
+                Vector3 startPosition = new Vector3(mMap.Start.Position.x, mMap.Start.Position.y + 10, mMap.Start.Position.z);
+
+               
+            }
+        }
+    }
+
+    public void Generate()
+    {
+        mMap.GenerateWorld();
+    }
+
+    public void Exit()
+    {
+#if !UNITY_EDITOR
+        Application.Quit();
+#endif
+    }
+}
